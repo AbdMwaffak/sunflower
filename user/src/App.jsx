bc64c4f68bf5d46cac9f358dcadb4f77e9656ccc
Abd Abouhawa <abdabouhawa@gmail.com>
2025-02-08 05:38:39 +0400

    😁

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 import ScrollToTop from './ScrollToTop';
-import { BrowserRouter as Router, Route, Routes } from 'react-router'
-import Home from './pages/home/Home'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from 'react-router';
+import Home from './pages/home/Home';
 import MyNav from './allExtensions/nav/MyNav';
 import Naturalflowers from './pages/naturalflowers/Naturalflowers';
 import ProductsByCategory from './pages/productsByCategory/ProductsByCategory';
@@ -29,64 +34,73 @@ import { useTranslation } from 'react-i18next';
 import AppFooter from './allExtensions/appFooter/AppFooter';
 import ArticlesById from './pages/articles/ArticlesById';
 
-
 function AppUser() {
   const cookies = new Cookies();
   const [num, setNum] = useState(true);
   const numCounter = () => {
-    setNum(!num)
-  }
+    setNum(!num);
+  };
   ////////////////////////////////////
   const { t, i18n } = useTranslation();
-  const lng = cookies.get("i18next") || "en";
+  const lng = cookies.get('i18next') || 'en';
   ///////////////////////
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   ///////////////////////
   useEffect(() => {
     window.document.dir = i18n.dir();
-  }, [dispatch, lng])
-  //////////////////// 
+  }, [dispatch, lng]);
+  ////////////////////
 
   return (
-    <div className="App">
-      <Router  
-       future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true, }}
->
-        
+    <div className='App'>
+      <Router
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <MyNav num={num} />
         <ScrollToTop />
         <Routes>
+          <Route path='*' element={<StaticFileCheck />} />
           {/* <Route path="/*" element={<NoMatch />} /> */}
-          <Route path="">
-    <Route index element={<Home numCounter={numCounter} />} />
-    <Route path="*" element={<NoMatch />} />
-    </Route>
+          <Route path=''>
+            <Route index element={<Home numCounter={numCounter} />} />
+            <Route path='*' element={<NoMatch />} />
+          </Route>
           <Route path='/' element={<Home numCounter={numCounter} />} />
 
-            {/* <Route path="/">
+          {/* <Route path="/">
    <Route index element={<Perfumes numCounter={numCounter} />} />
    <Route path="Perfumes" element={<Perfumes numCounter={numCounter} />} />
   </Route> */}
-         <Route path='/Perfumes' element={<Perfumes numCounter={numCounter} />} />
-           <Route path='/NaturalFlowers' element={<Naturalflowers numCounter={numCounter} />} />
-          <Route path='product/:id' element={<ProductProfile numCounter={numCounter} />} />
-    
+          <Route
+            path='/Perfumes'
+            element={<Perfumes numCounter={numCounter} />}
+          />
+          <Route
+            path='/NaturalFlowers'
+            element={<Naturalflowers numCounter={numCounter} />}
+          />
+          <Route
+            path='product/:id'
+            element={<ProductProfile numCounter={numCounter} />}
+          />
+
           {/* <Route path='category/:id' element={<ProductsByCategory />} /> */}
-          <Route path="category">
-          <Route index element={<ProductsByCategory />} />
-    <Route path=":id" element={<ProductsByCategory />} />
-    </Route>
+          <Route path='category'>
+            <Route index element={<ProductsByCategory />} />
+            <Route path=':id' element={<ProductsByCategory />} />
+          </Route>
           <Route path='/Articles' element={<Articles />} />
           {/* <Route path='/Article/:id' element={<ArticlesById />} /> */}
-          <Route path="Article">
-          <Route index element={<ArticlesById />} />
-    <Route path=":id" element={<ArticlesById />} />
-    </Route>
+          <Route path='Article'>
+            <Route index element={<ArticlesById />} />
+            <Route path=':id' element={<ArticlesById />} />
+          </Route>
           <Route path='/ContactUs' element={<ContactUs />} />
           <Route path='/AboutTheStore' element={<About />} />
-      
+
           <Route element={<ProtectedRoutes />}>
             <Route path='/StoreMessages' element={<Messages />} />
             <Route path='/MyAccount/:userName' element={<MyAccount />} />
@@ -99,8 +113,21 @@ function AppUser() {
           <Route path='/Register' element={<Register />} />
         </Routes>
         <AppFooter />
-      </Router >
-    </div >
+      </Router>
+    </div>
   );
 }
 export default AppUser;
+
+function StaticFileCheck() {
+  const location = useLocation();
+
+  // If the requested URL ends with .html, prevent React Router from handling it
+  if (location.pathname.endsWith('.html')) {
+    window.location.href = location.pathname;
+    return null;
+  }
+
+  // Otherwise, render the NoMatch page (or any other fallback)
+  return <NoMatch />;
+}
